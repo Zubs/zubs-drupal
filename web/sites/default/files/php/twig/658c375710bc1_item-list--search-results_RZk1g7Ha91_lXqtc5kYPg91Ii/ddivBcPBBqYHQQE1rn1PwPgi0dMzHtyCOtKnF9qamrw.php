b25414c660dcadabbfe8ad32b3ed40d60373f5f8
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2023-12-29 00:18:26 +0000

    feat: init first module

---
+++ web/sites/default/files/php/twig/658c375710bc1_item-list--search-results_RZk1g7Ha91_lXqtc5kYPg91Ii/ddivBcPBBqYHQQE1rn1PwPgi0dMzHtyCOtKnF9qamrw.php
@@ -0,0 +1,109 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/claro/templates/classy/dataset/item-list--search-results.html.twig */
+class __TwigTemplate_f230674b3e084302f1f8a3328e009a43 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "item-list.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 24
+        $context["classes"] = ["search-results", ($this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source,         // line 26
+($context["context"] ?? null), "plugin", [], "any", false, false, true, 26), 26, $this->source) . "-results")];
+        // line 29
+        $context["attributes"] = twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [($context["classes"] ?? null)], "method", false, false, true, 29);
+        // line 1
+        $this->parent = $this->loadTemplate("item-list.html.twig", "core/themes/claro/templates/classy/dataset/item-list--search-results.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["context"]);    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "core/themes/claro/templates/classy/dataset/item-list--search-results.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array (  48 => 1,  46 => 29,  44 => 26,  43 => 24,  36 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/claro/templates/classy/dataset/item-list--search-results.html.twig", "/Users/zubair/Library/Mobile Documents/com~apple~CloudDocs/Desktop/drupal/web/core/themes/claro/templates/classy/dataset/item-list--search-results.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 24);
+        static $filters = array();
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set'],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
